8de2630e52205e9e5317436faa5baef8919835aa
Martin <maa@CARR.panter.local>
2008-11-20 12:01:15 +0100

    changed photo order

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
 
   validates_uniqueness_of :title
-  has_many :photos, :order => "updated_at", :dependent => :destroy
+  has_many :photos, :order => "updated_at DESC", :dependent => :destroy
 
 end
